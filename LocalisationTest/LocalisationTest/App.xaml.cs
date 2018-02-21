be00f5f5e29c0b0ed21b9cabeaa2f53785cf5459
Andrew Robinson <Andrew.J.Robinson@henderson.com>
2018-02-21 11:24:18 +0000

    Add project files.

---
+++ LocalisationTest/LocalisationTest/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using LocalisationTest.Resources;
+using Plugin.Multilingual;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace LocalisationTest
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+            CrossMultilingual.Current.CurrentCultureInfo = new CultureInfo("cy");
+            AppResources.Culture = CrossMultilingual.Current.CurrentCultureInfo;
+            MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ LocalisationTest/LocalisationTest/Helpers/TranslateExtension.cs
@@ -0,0 +1,40 @@
+﻿using Plugin.Multilingual;
+using System;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalisationTest.Helpers
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        const string ResourceId = "LocalisationTest.Resources.AppResources";
+
+        static readonly Lazy<ResourceManager> _resourceManager = 
+            new Lazy<ResourceManager>(() => new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly));
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return "";
+
+            var cultureInfo = CrossMultilingual.Current.CurrentCultureInfo;
+            var translation = _resourceManager.Value.GetString(Text, cultureInfo);
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    String.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, cultureInfo.Name),
+                    "Text");
+#else
+				translation = Text; // returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+    }
+}
